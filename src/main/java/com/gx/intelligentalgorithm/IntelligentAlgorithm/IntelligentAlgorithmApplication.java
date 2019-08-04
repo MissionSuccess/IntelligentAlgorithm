f340bf77285037b624d8e384bd78534c524769e7
GuoXin <guoxin9682@qq.com>
2019-08-04 10:38:09 +0800

    第一次提交

---
+++ src/main/java/com/gx/intelligentalgorithm/IntelligentAlgorithm/IntelligentAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.gx.intelligentalgorithm.IntelligentAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentAlgorithmApplication.class, args);
+	}
+
+}
